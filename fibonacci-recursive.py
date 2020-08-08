5775eee96be3454e03be3c6f8a32f4292c666bb3
himangi6550 <56739294+himangi6550@users.noreply.github.com>
2020-08-08 11:16:54 +0530

    Update fibonacci-recursive.py

--- fibonacci-recursive.py
+++ fibonacci-recursive.py
@@ -1,12 +1,13 @@
-#to print the fibonacci series
+#to print the fibonacci series till n
 def fibonacci(a,b):
     c=a+b
-    if c<48:
+    if c<n:
         print(c,end=" ")
         fibonacci(b,c)
          
 a=0
 b=1
+n=int(input("Enter the value of n: "))
 print("Fibonacci Series")
 print(a,b,end=" ")
 fibonacci(a,b)
@@ -14,7 +15,7 @@ fibonacci(a,b)
 
 
 #to print the nth term
-num=int(input("\nenter the nth term"))
+num=int(input("\nenter the nth term: "))
 
 def fibonacci(n):
     if n==1:
